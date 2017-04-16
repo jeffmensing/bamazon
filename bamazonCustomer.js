8d4630af934a721ffd8badcc977caca5d0c15b12
Jeff Mensing <Jeff Mensing>
2017-04-15 22:14:51 -0500

    updating bamazonCustomer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,55 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var prompt = require("prompt");
+var colors = require('colors/safe');
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "5tgb^YHN",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+//connect to the mysql database and pull the information from the Products database to display to the user
+connection.query('select item_id, product_name, price from products', function(err, result){
+	if(err) console.log(err);
+
+	//creates a table for the information from the mysql database to be placed
+	var table = new Table({
+		head: ['Item#', 'Product Name', 'Price'],
+		style: {
+			head: ['blue'],
+			compact: false,
+			colAligns: ['center'],
+		}
+	});
+
+	//loops through each item in the mysql database and pushes that information into a new row in the table
+	for(var i = 0; i < result.length; i++){
+		table.push(
+			[result[i].item_id, result[i].product_name, result[i].price]
+		);
+	}
+	console.log(table.toString());
+
+
+});
+
+
+
+
+
+
+
+
+
+
