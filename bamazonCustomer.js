8f0339318a4e52af8d77b9d1346ed5ba9a59f43a
Jeff Mensing <Jeff Mensing>
2017-04-18 08:17:43 -0500

    updated bamazoncutomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,51 +5,45 @@ var colors = require('colors/safe');
 var Table = require('cli-table');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  // Your username
-  user: "root",
-  // Your password
-  password: "5tgb^YHN",
-  database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "",
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
 
-//connect to the mysql database and pull the information from the Products database to display to the user
-connection.query('select item_id, product_name, price from products', function(err, result){
-	if(err) console.log(err);
+function displayInventory() {
+    //connect to the mysql database and pull the information from the Products database to display to the user
+    connection.query('select item_id, product_name, price from products', function(err, result) {
+        if (err) console.log(err);
+        //creates a table for the information from the mysql database to be placed
+        var table = new Table({
+            head: ['Item#', 'Product Name', 'Price'],
+            style: {
+                head: ['blue'],
+                compact: false,
+                colAligns: ['center'],
+            }
+        });
+        //loops through each item in the mysql database and pushes that information into a new row in the table
+        for (var i = 0; i < result.length; i++) {
+            table.push(
+                [result[i].item_id, result[i].product_name, result[i].price]
+            );
+        }
+        console.log(table.toString());
+    });
+}
 
-	//creates a table for the information from the mysql database to be placed
-	var table = new Table({
-		head: ['Item#', 'Product Name', 'Price'],
-		style: {
-			head: ['blue'],
-			compact: false,
-			colAligns: ['center'],
-		}
-	});
+function purchas() {
 
-	//loops through each item in the mysql database and pushes that information into a new row in the table
-	for(var i = 0; i < result.length; i++){
-		table.push(
-			[result[i].item_id, result[i].product_name, result[i].price]
-		);
-	}
-	console.log(table.toString());
+}
 
-
-});
-
-
-
-
-
-
-
-
-
-
+displayInventory();
